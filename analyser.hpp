4c4c2663e43432014f689bd3fbf1089490e2d841
Tim Jason Weber <timwweberj@gmail.com>
2024-02-17 15:24:55 +0100

    Created new function is_identical_filenames, takes two string vectors, converts each entry to filesystem::path and compares the filenames of each entry. Returns 0 for failure, 1 for is_identical.

--- analyser.hpp
+++ analyser.hpp
@@ -6,6 +6,8 @@
 #include <vector>
 #include <filesystem>
 #include <algorithm>
+#include <stdexcept>
+#include <fstream>
 
 class analyser{
     private:
@@ -15,7 +17,37 @@ class analyser{
         analyser(const std::vector<std::string>& path, const std::string& name) : programPaths(path), programName(name) {}
 
         const std::string backupDir = "C:\\Data\\TW\\Software\\Coding\\ConfigSync\\ConfigArchive\\" + programName;
-        std::string getLastBackupPath(){
+        
+
+        void recurse_scanner(const std::filesystem::path& dirPath, std::vector<std::string>& itemVector){ // Self referencing function. itemVector parameter passed in as reference, to modify original object directly.
+            for(const auto& entry : std::filesystem::directory_iterator(dirPath)){
+                if(entry.is_directory()){
+                    recurse_scanner(entry.path(), itemVector);
+                }
+                else{
+                    itemVector.push_back(entry.path().string());
+                }
+            }
+        }
+        
+        int is_identical_filenames(const std::vector<std::string>& base, const std::vector<std::string>& reference){
+            int i = 0;
+            for(const auto& entry : base){
+                std::filesystem::path entry_fs = entry;
+                std::filesystem::path reference_fs = reference[i];
+
+                if(entry_fs.filename() != reference_fs.filename()){
+                    return 0;
+                }
+
+                i++;
+            }
+            return 1;
+        }
+
+        int byte_comparison();
+
+        std::string get_last_backup_path(){
             const std::filesystem::path backupDirFs = backupDir;
 
             std::vector<std::string> dateDirs;
@@ -29,33 +61,32 @@ class analyser{
             return dateDirs[0];
         }
 
-        void recurseScanner(const std::filesystem::path& dirPath, std::vector<std::string>& itemVector){ // Self referencing function. itemVector parameter passed in as reference, to modify original object directly.
-            for(const auto& entry : std::filesystem::directory_iterator(dirPath)){
-                if(entry.is_directory()){
-                    recurseScanner(entry.path(), itemVector);
-                }
-                else{
-                    itemVector.push_back(entry.path().string());
-                }
-            }
-        }
-
-        std::string checkDiff(){
-            const std::string lastSavedDir = getLastBackupPath();
+        std::string check_diff(){
+            const std::string lastSavedDir = get_last_backup_path();
             const std::filesystem::path savedConfigPath = backupDir + "\\" + lastSavedDir;
             
             // Scan saved config directory recursively
             std::vector<std::string> savedConfigItems;
-            recurseScanner(savedConfigPath, savedConfigItems);
+            recurse_scanner(savedConfigPath, savedConfigItems);
 
             
             // Scan each item of ProgramPaths recursively
             std::vector<std::string> currentConfigItems; 
             for(const auto& item : programPaths){
-                recurseScanner(item, currentConfigItems);
+                recurse_scanner(item, currentConfigItems);
             }
 
             // Compare vectors
+            if(is_identical_filenames(savedConfigItems, currentConfigItems)){
+                
+            }
+            else{
+                std::string matchError = "Error: (checkDiff) entries do not match!"; // For debugging only
+                throw std::runtime_error(matchError);
+
+                // --> structure changed, new save config
+            }
+
             // Load last_write_time in multilevel vector
         }
 };
