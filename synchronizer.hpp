bda3be054b18bc9cbbcb8b97f1039023ea58b3ad
Tim Jason Weber <timwweberj@gmail.com>
2024-02-18 01:40:39 +0100

    Started working on synchronizer header. Copy config function done.

--- synchronizer.hpp
+++ synchronizer.hpp
@@ -4,10 +4,26 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <filesystem>
 
-class synchronize{
+class synchronizer{
     private:
-        std::vector<std::string> configItemsSaved;
-        std::vector<std::string> configItemsCurrent;
+        const std::vector<std::string> programPaths;
+        const std::string backupPath;
+    public:
+        synchronizer(const std::vector<std::string>& path1, const std::string path2) : programPaths(path1), backupPath(path2) {}
+
+        void copy_config(){
+            for(const auto& item : programPaths){
+                const std::filesystem::path source = item;
+
+                try{
+                    std::filesystem::copy(source, backupPath + "\\" + source.parent_path().string() + "\\" + source.filename().string());
+                }
+                catch(std::filesystem::filesystem_error& copyError){
+                    throw std::runtime_error(copyError);
+                }
+            }
+        }
 };
 #endif
